36248a94b8f0bcc50d76b8bc2aee3f2287349893
Jeremy Bernard <jeremy@bernard.sh>
2022-07-26 11:23:55 +0200

    Update pokedex + add pokedex fix

---
+++ data/sanitize_pokedex.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const basePokedex = require('./pokedex.json');
+const baseTypes = require('./types.json');
+
+const newPokedex = basePokedex.map((pokemonData) => ({
+  ...pokemonData,
+  name: pokemonData.name.french,
+  stats: pokemonData.base,
+  types: pokemonData.type.map((type) => baseTypes.find(
+    (currentBaseType) => currentBaseType.english === type,
+  ).french),
+})).map((pokemonData) => {
+  const currentPokemon = pokemonData;
+  delete currentPokemon.type;
+  delete currentPokemon.base;
+
+  return currentPokemon;
+});
+
+const jsonContent = JSON.stringify(newPokedex);
+fs.writeFile('output.json', jsonContent, 'utf8', (err) => {
+  if (err) {
+    console.log('An error occured while writing JSON Object to File.');
+    console.log(err);
+    return;
+  }
+
+  console.log('JSON file has been saved.');
+});

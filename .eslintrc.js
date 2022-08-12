defc57e54e5072a02089140d2465e1374f79920f
Jeremy Bernard <jeremy@bernard.sh>
2022-08-12 12:00:56 +0200

    Enable TS & Add TS support in EsLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
   },
+  plugins: ['import'],
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+    project: './tsconfig.eslint.json',
   },
   rules: {
   },

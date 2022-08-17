99aa2bc566163f37a46e747038b435ac2a9685e1
Jeremy Bernard <jeremy@bernard.sh>
2022-08-17 10:49:17 +0200

    Add dump json route in HTTP controller

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,14 +1,23 @@
 import { Application } from 'express';
+import { readFileSync } from 'fs';
 import towerResetController from './towerReset';
 
 const {
   HTTP_ROUTE_SUFFIX,
+  JSON_DB_FILE,
 } = process.env;
 
 const setupHttpRoutes = (app: Application) => {
   app.get(`/tower/reset/${HTTP_ROUTE_SUFFIX}`, (req, res) => {
     towerResetController.execute();
     res.send('OK');
+  });
+
+  app.get(`/json/${HTTP_ROUTE_SUFFIX}`, (req, res) => {
+    const rawdata = readFileSync(JSON_DB_FILE || './data.json');
+    const jsonData = JSON.parse(rawdata.toString());
+
+    res.json(jsonData);
   });
 
   app.get(`/ping/${HTTP_ROUTE_SUFFIX}`, (req, res) => {

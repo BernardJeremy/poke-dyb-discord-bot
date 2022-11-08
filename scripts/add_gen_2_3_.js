56b525c3278ea6281eaba9a9c85322c797e1f537
Jeremy Bernard <jeremy@bernard.sh>
2022-11-08 11:13:53 +0100

    Add Gen 2 & 3 to Pokedex

---
+++ scripts/add_gen_2_3_.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const basePokedex = require('./output.json');
+const craftingRarityData = require('./crafting_rarity_gen-2_3.json');
+
+const newPokedex = basePokedex.map((pokemonData) => ({
+  ...pokemonData,
+  rarityLevel: craftingRarityData.find((data) => data.id === pokemonData.id).rarityLevel || 0,
+  craftingPrice: craftingRarityData.find((data) => data.id === pokemonData.id).craftingPrice || -1,
+}));
+
+const jsonContent = JSON.stringify(newPokedex);
+fs.writeFile('output.json', jsonContent, 'utf8', (err) => {
+  if (err) {
+    console.log('An error occured while writing JSON Object to File.');
+    console.log(err);
+    return;
+  }
+
+  console.log('JSON file has been saved.');
+});

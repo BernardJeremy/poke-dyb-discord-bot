4cbd9558316bb7fdafee8f42b1c301435aad7e21
Jeremy Bernard <BernardJeremy@users.noreply.github.com>
2023-04-19 15:22:34 +0200

    Add more security in dez cmd

--- src/commands/dez.ts
+++ src/commands/dez.ts
@@ -50,6 +50,11 @@ export default {
       message.reply(`Tu n'as que ${targetPokemonList.length} exemplaire(s) de ce pokemon`);
       return;
     }
+   
+    if (nbrToDez === targetPokemonList.length) {
+      message.reply(`Tu ne peux pas dez le dernier exemplaire d'un pokemon (et l'assurance ne rembourse plus)`);
+      return;
+    }
 
     user.pokedex = removePokemonFromList(user.pokedex, pokemonNbr, nbrToDez);
 
